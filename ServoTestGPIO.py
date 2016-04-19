858ca9cce0740127871931773a709c36c8240368
hermstadalex <hermstadalex@gmail.com>
2016-04-18 20:02:40 -0700

    Removed unnecessary Servo.py and Servo1.py

--- ServoTestGPIO.py
+++ ServoTestGPIO.py
@@ -5,7 +5,7 @@ Purpose: Control the Servo machines connected to PCA9685 driver onboard of Navio
 Requirements: Adafruit I2C and PCA9685 PWM generator drivers
 Use: Connect a PWM input device to the Navio+ servo rail then run "sudo python Servo1.py"
 """
-/* ---- Includes ---- */
+# ---- Includes ---- 
 from Adafruit_PWM_Servo_Driver import PWM
 import time
 import math
@@ -14,18 +14,18 @@ import sys
 
 sys.path.append("..")
 from Navio import GPIO
-/* ---- End Includes ---- */
+# ---- End Includes ---- 
 
-/* ---- Setup Modules ---- */
+# ---- Setup Modules ---- 
 #drive Output Enable in PCA low
 pin = GPIO.Pin(27)
 pin.write(0)
 
 PCA9685_DEFAULT_ADDRESS = 0x40
 frequency = 50
-/* ---- End Module Setup ---- */
+# ---- End Module Setup ---- 
 
-/* ---- Configure Outputs ---- */
+# ---- Configure Outputs ---- 
 NAVIO_RCOUTPUT_1 = 3
 SERVO_MIN_ms = 1.250 # mS
 SERVO_MAX_ms = 1.750 # mS
@@ -38,7 +38,7 @@ pwm = PWM(0x40, debug=False)
 
 # Set frequency to 60 Hz
 pwm.setPWMFreq(frequency)
-/* ---- End Output Configuration ---- */
+# ---- End Output Configuration ---- 
 
 while(True):
 	pwm.setPWM(NAVIO_RCOUTPUT_1, 0, SERVO_MIN);
