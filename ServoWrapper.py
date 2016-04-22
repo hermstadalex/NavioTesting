e66e804a85a1b74f4308c90788963e75c81bef25
Alexander Guthrie <alexanderg100@gmail.com>
2016-04-22 00:45:10 -0700

    Recreated the wrapper's functioning math using info from todays tests

--- ServoWrapper.py
+++ ServoWrapper.py
@@ -40,6 +40,9 @@ pin.write(0)
 This assignment adresses the PCA9685BS register controlling LED14_OFF_L 
 Hex constants are prefixed with "0x"
 hex(0x40) = dec(64) = bin(01000000)
+
+Also the PCA9685 analog output wave can range from 24Hz-1526Hz frequency
+and the Savox Servo likes to receive PWM frequencies of 255H0-330Hz.
 ''' 
 PCA9685_DEFAULT_ADDRESS = 0x40
 frequency = 300
@@ -48,37 +51,61 @@ period = 1.0/frequency
 NAVIO_RCOUTPUT_1 = 3
 # ---- End Module Setup ---- 
 
-
-# ---- Set Servo Degrees of Movement ----
-SERVO_deg1 = 1 #don't set this to zero
-SERVO_deg2 = 100
-# ---- End Set Servo DOM ----
-
-
-# ---- Convert Outputs ----
+# ---- Set & Convert Outputs ----
 '''
 A digital signal controls the PCA9685 which prepares an analog PWM wave
 for the Savox SC-1258TG servo.
 
-The PCA9685 is controlled by I2C and each of the 16 drivers has 12-bit
-PWM output which is 4096 steps. The analog output wave can range from
-24Hz-1526Hz frequency with possible 0%-100% duty cycle.
+The PCA9685 is controlled by writing 12bits over I2C to one of the 16
+ output drivers. These 12 bits control the generated output pulse width.
+ The signal's absolute width, not its duty cycle, directly controls the
+ servo's angle.
+Emperical testing on April 4, 2016 shows that in order to maintain a
+ a constant signal width, the value of the 12 bits written must change
+ with respect to the operating frequency. The specific relation:
+
+		12BitWritten = 12BitRange * (DesiredSignalWidth * OperatingFrequency)
+
+ Notice the ratio of the PWM width to the operating frequency, it is
+ used to scale the total bit range.
+ Example 1) Output minimum PWM width that moves (cw direction) the servo
+ 
+		SERVO_moveMin = math.trunc ((MinWidth*frequency*4096.0)-1)
+		
+ Example 2) Output maximum PWM width that moves (ccw direction) the servo
+ 
+		SERVO_moveMin = math.trunc ((MaxWidth*frequency*4096.0)-1)
+
 '''
-SERVO_MAX_deg = 100
+PWM_MinWidth = 0.000675 #S. This was calculated on April 4, 2016
+#PWM_NeutWidth = 0.0015 #S. This is informed from part documentation
+PWM_MaxWidth = 0.002425 #S. This is a guess
+PWM_Range = PWM_MaxWidth - PWM_MinWidth #0.00175
 
-#ABS_MIN_PW = 0.001 #S or 1000 uS correspond to 0 degrees and cw
-#ABS_NEUT_PW = 0.0015 #S or 1500 uS
-#SERVO_MAX_PW = 0.002 #S or 2000 uS correspond to 100 degrees and ccw
+# Define servo degrees of movement
+SERVO_Range = 180
 
-#Convert degree percentage to 0-4096 scale:
-SERVO_move1 = math.trunc( ((SERVO_deg1/SERVO_MAX_deg) * 4096.0) -1)
-SERVO_move2 = math.trunc( ((SERVO_deg2/SERVO_MAX_deg) * 4096.0) -1)
+# Set desired servo positions
+SERVO_deg1 = 0
+SERVO_deg2 = 180
 
+# Convert our 180 degree positions to PWM widths in seconds
+PWM_Width1 = PWM_MinWidth + (PWM_Range * (SERVO_deg1/SERVO_Range))
+PWM_Width2 = PWM_MinWidth + (PWM_Range * (SERVO_deg2/SERVO_Range))
+#WM_Width1 = PWM_MaxWidth - (PWM_Range * (1 - (SERVO_deg1/SERVO_Range))) #This is a messy alternative to the line above
+#PWM_Width2 = PWM_MaxWidth - (PWM_Range * (1 - (SERVO_deg2/SERVO_Range)))
+
+#Convert PWM widths to 12 bits (a scale of 0-4095) to write to the PCA9685.
+SERVO_move1 = math.trunc((4096.0 * PWM_Width1 * frequency) -1)
+SERVO_move2 = math.trunc((4096.0 * PWM_Width2 * frequency) -1)
+
+# Set PWM register address
 pwm = PWM(0x40, debug=False)
 
-# Set frequency to 50 Hz
+# Set output pulse frequency of the PCA9685
 pwm.setPWMFreq(frequency)
-# ---- End Convert Outputs ---- 
+
+# ---- End Set & Convert Outputs ----
 
 while(True):
 	try:
