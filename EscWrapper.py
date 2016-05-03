fbf704bc510f2aa8ed7d0f802db7eb8d72c3abd6
Navio+1 <roboieeeucsd@gmail.com>
2016-05-02 21:35:39 -0700

    Changed values to match Oscope readings from radio receiver, still doesn't work

--- EscWrapper.py
+++ EscWrapper.py
@@ -3,7 +3,7 @@ Robotritons testing of the Xerun SCT-Pro ESC
 
 Purpose: Control the ESC that powers the motor
 Requirements: Adafruit I2C and PCA9685 PWM generator drivers
-Use: Connect a PWM input device to the Navio+ servo rail then run "sudo python EscWrapper.py"
+Use: Connect the Xerun ESC to the Navio+ servo rail then run "sudo python EscWrapper.py"
 
 Resources:
 http://www.societyofrobots.com/robotforum/index.php?topic=4299.0
@@ -11,7 +11,7 @@ http://robots.dacloughb.com/project-2/esc-calibration-programming/ (edited)
 http://aeroquad.com/showthread.php?5502-Which-PWM-frequency-do-I-use-to-control-my-ESC (edited)
 """
 # ---- Includes ---- 
-from navio.adafruit_PWM_servo_driver import PWM
+from navio.adafruit_pwm_servo_driver import PWM
 import time
 import math
 
@@ -26,47 +26,53 @@ navio.util.check_apm()
 
 # ---- Setup Modules ----
 '''
-By driving Output Enable LOW outputs are activated
+Enable PWM outputs.
+Drive the PCA9685BS model HVQFN28's OE pin #20 (27 RPI2) to "LOW"
 '''
 pin = navio.gpio.Pin(27)
 pin.write(0)
 
 '''
-Adresses the PCA9685BS register controlling LED14_OFF_L 
+Adresses the PCA9685BS register controlling LED14_OFF_L
 ''' 
 PCA9685_DEFAULT_ADDRESS = 0x40
-frequency = 300
+frequency = 45.05 #45.04 is 49.80Hz and 45.05 is above 50.15Hz
 period = 1.0/frequency
 
-NAVIO_RCOUTPUT_1 = 3
+NAVIO_RCOUTPUT_3 = 5
 # ---- End Module Setup ---- 
 
 # ---- Set & Convert Outputs ----
 '''
-A digital signal controls the PCA9685 which prepares an analog PWM wave
-for the Savox SC-1258TG servo.
+A 12 bit digital I2C signal controls the PCA9685 which then prepares an
+analog PWM wave for the Xerun SCT-Pro ESC.
+The PWM signal's absolute width directly controls the motor speed.
+Emperical testing on April 4, 2016 shows that in order to maintain a
+constant signal width, the value of the 12 bits written must change
+with respect to the operating frequency. The specific relation:
+
+	12BitWritten = 12BitRange * (DesiredSignalWidth * OperatingFrequency)
 '''
-PWM_MinWidth = 0.000675 #S. This was calculated on April 4, 2016
-#PWM_NeutWidth = 0.0015 #S. This is informed from part documentation
-PWM_MaxWidth = 0.002425 #S. This is a guess
-PWM_Range = PWM_MaxWidth - PWM_MinWidth #0.00175
+PWM_MinWidth = 0.001158 #S. This was measured on May 1, 2016.
+PWM_MaxWidth = 0.001914 #S. This was measured on May 1, 2016.
+PWM_Range = PWM_MaxWidth - PWM_MinWidth #0.000756
+PWM_stop = 0.001534 #S. To stop the motor, send a constant stop pulse.
 
 # Define motor max speed
-Motor_Range = 180
+Motor_Range = 100.0
 
 # Set desired motor speeds
 Motor_speed1 = 0
 Motor_speed2 = 100
 
 # Convert our 180 degree positions to PWM widths in seconds
-PWM_Width1 = PWM_MinWidth + (PWM_Range * (Motor_deg1/Motor_Range))
-PWM_Width2 = PWM_MinWidth + (PWM_Range * (Motor_deg2/Motor_Range))
-#WM_Width1 = PWM_MaxWidth - (PWM_Range * (1 - (SERVO_deg1/SERVO_Range))) #This is a messy alternative to the line above
-#PWM_Width2 = PWM_MaxWidth - (PWM_Range * (1 - (SERVO_deg2/SERVO_Range)))
+PWM_Width1 = PWM_stop + (PWM_Range * (Motor_speed1/Motor_Range))
+PWM_Width2 = PWM_stop + (PWM_Range * (Motor_speed2/Motor_Range))
 
 #Convert PWM widths to 12 bits (a scale of 0-4095) to write to the PCA9685.
-Motor_move1 = math.trunc((4096.0 * PWM_Width1 * frequency) -1)
+Motor_move1 = math.trunc((4096.0 * 0.0021 * frequency) -1) #Changed this on May 2, 2016 to get about 1.91ms output
 Motor_move2 = math.trunc((4096.0 * PWM_Width2 * frequency) -1)
+print Motor_move1
 
 # Set PWM register address
 pwm = PWM(0x40, debug=False)
@@ -78,9 +84,9 @@ pwm.setPWMFreq(frequency)
 
 while(True):
 	try:
-		pwm.setPWM(NAVIO_RCOUTPUT_1, 0, Motor_move1);
-		time.sleep(1);
-		pwm.setPWM(NAVIO_RCOUTPUT_1, 0, Motor_move2);
-		time.sleep(1);
+		pwm.setPWM(NAVIO_RCOUTPUT_3, 0, Motor_move1);
+		time.sleep(5);
+		#pwm.setPWM(NAVIO_RCOUTPUT_3, 0, Motor_move2);
+		#time.sleep(5);
 	except KeyboardInterrupt:
 		sys.exit()
