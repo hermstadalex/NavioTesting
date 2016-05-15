f108d27e8f709d9412b0a5780d26d57451efd76e
Wilfred <wilfredkwan91801@gmail.com>
2016-05-15 14:08:28 -0700

    made GPS Testing File

---
+++ GPSTest.py
@@ -0,0 +1,24 @@
+"""
+Robotritons testing version of steering controls.
+Based on iterations of the Emlid Servo.py example.
+
+Purpose: 
+Requirements:
+Use: 
+
+Resources:
+https://shahriar.svbtle.com/importing-star-in-python
+"""
+
+from GPSBackEnd import *
+
+ubl = U_blox()
+for ind in range(0, 10):
+	ubl.enable_posllh()
+while(1):
+	buffer = ubl.bus.xfer2([100])
+	for byt in buffer:
+		ubl.scan_ubx(byt)
+		if(ubl.mess_queue.empty() != True):
+			msg = ubl.parse_ubx()
+			if (msg != None): print(msg)
